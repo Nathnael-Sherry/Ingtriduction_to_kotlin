2a45eec029f2627b0c228a56ea74ecd4c5892bec
SalomeNderu <sallienderu@gmail.com>
2023-03-14 10:23:36 +0300

    intro to kotlin

---
+++ src/main/kotlin/kotlin rectangle.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+
+    var length = 50
+    var width = 25
+
+    var results = length * width
+    print(results)
+}
---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+
+    var num1 = 100
+    var num2 = 250
+
+    var results = num1 + num2
+    print(results)
+}
