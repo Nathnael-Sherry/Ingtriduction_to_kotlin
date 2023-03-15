f676961ceca01a2e2c77610d905e1e955d1589ad
SalomeNderu <sallienderu@gmail.com>
2023-03-15 08:54:39 +0300

    Kotlin datatypes

---
+++ src/main/kotlin/BMI calculator.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    var weight = 123
+    var height = 34
+
+}
---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+//    Int
+    var age = 20
+    var age:Int = 20
+
+//    strings
+    var name = "greg"
+    var name:String = "Dj Bash"
+
+//    Char
+    var cit = 'J'
+
+//    Booleans
+    var status = true
+    var status = false
+
+//    Double
+    var temp = 36.55
+
+//    Float
+    var lat:Float = 36.7F
+
+
+}
