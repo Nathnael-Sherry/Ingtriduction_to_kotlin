4325a342fe55c8e390ced9dd8648ba50b7214968
SalomeNderu <sallienderu@gmail.com>
2023-03-15 09:42:12 +0300

    camparison exercise

---
+++ src/main/kotlin/Ex_compare.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    print("Enter your code")
+    var number:Int = readLine()!!.toInt()
+
+    if (number < 1) {
+        print("1.5GB for 3hours @Ksh50")
+    }else if (number == 1) {
+        print("Amazing + Bazu Data")
+    } else if (number == 2) {
+        print("Tubonge + ALLNET")
+    }else if (number == 3){
+        print("Unliminet Combo")}
+    else{
+        print("Kopa")
+    }
+}
