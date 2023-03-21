d8d3721530fd373a896e352e58f00a9f57f75869
SalomeNderu <sallienderu@gmail.com>
2023-03-21 09:44:31 +0300

    Functions

---
+++ src/main/kotlin/Ex_Function.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    add()
+    multiply()
+    divide()
+}
+
+fun add(){
+    var num1 =650
+    var num2 = 150
+    var result = num1 + num2
+     println(result)
+}
+fun multiply(){
+    var num1 =100
+    var num2 = 20
+    var result = num1 * num2
+    println(result)
+}
+fun divide(){
+    var num1 =100
+    var num2 = 5
+    var result = num1 / num2
+    println(result)
+}
---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    sayhello()
+    today()
+}
+
+fun sayhello(){
+    println("GoodMorning peopple")
+}
+
+fun today(){
+    println("Lets Go Out")
+}
